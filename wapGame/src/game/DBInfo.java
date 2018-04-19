10a659165e151650dde015a5b8d768426656f7db
Mateusz <rasp.berry.kti@gmail.com>
2018-04-19 14:58:42 +0200

    ..

--- wapGame/src/game/DBInfo.java
+++ wapGame/src/game/DBInfo.java
@@ -2,8 +2,8 @@ package game;
 
 public class DBInfo {
 	
-	//static String host = "jws-app-mysql"; // The standard name in Tomcat+ MySQL on openshift
-	static String host = "localhost";
+	static String host = "jws-app-mysql"; // The standard name in Tomcat+ MySQL on openshift
+	//static String host = "localhost";
 	
 	static String DBname = "lllc";
 	static int port = 3306; // This is for MySQL
